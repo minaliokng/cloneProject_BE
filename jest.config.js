db8c71d6e8b2a6711f07910f3834ca2787d530c8
GogumaYummy <sora1211x@gmail.com>
2022-12-23 21:25:42 +0900

    chore: 설정 파일 변경 및 추가
    타입스크립트 + ESLint 적용하기 위한 설정 변경

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,20 @@
-module.exports = {
-  testPathIgnorePatterns: ['/node_modules/', '/test/fixtures/'],
+export default {
   verbose: true,
+  testRegex: ['.*\\.test\\.(js|ts)$'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.json',
+        diagnostics: true,
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleDirectories: ['node_modules'],
+  preset: 'ts-jest',
+  setupFiles: ['dotenv/config'],
+  testMatch: null,
+  testEnvironment: 'node',
 };
