1f5a7aa4c46ce580fe274c1865a484d1adabab65
GogumaYummy <sora1211x@gmail.com>
2022-12-26 19:09:06 +0900

    feat: 인증 미들웨어 연결

--- src/routes/comments.route.ts
+++ src/routes/comments.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import CommentsController from '../controllers/comments.controller';
+import { requireLogin } from '../middlewares/auth.middleware';
 
 const commentsRouter = Router();
 const commentsController = new CommentsController();
 
-commentsRouter.post('/posts/:postId/comments', commentsController.createComment);
+commentsRouter.post('/posts/:postId/comments', requireLogin, commentsController.createComment);
 commentsRouter.get('/posts/:postId/comments', commentsController.getComments);
-commentsRouter.patch('/comments/:commentId', commentsController.updateComment);
-commentsRouter.delete('/comments/:commentId', commentsController.deleteComment);
+commentsRouter.patch('/comments/:commentId', requireLogin, commentsController.updateComment);
+commentsRouter.delete('/comments/:commentId', requireLogin, commentsController.deleteComment);
 
 export default commentsRouter;
