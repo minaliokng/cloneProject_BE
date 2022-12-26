b37a730490c927e4446a5b605a0631d65b780f96
TaeHyeongKwon <1004kth1313@naver.com>
2022-12-26 19:26:12 +0900

    feat: postCRUD

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,4 +1,6 @@
+import { Request, Response, NextFunction } from 'express';
 import PostsService from '../services/posts.service';
+import { postInputPattern, postIdPattern } from '../validations/posts.validation';
 
 class PostsController {
   postsService: PostsService;
@@ -6,6 +8,71 @@ class PostsController {
   constructor() {
     this.postsService = new PostsService();
   }
+
+  createPost = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // const postImage = req.file.key
+      const { userId } = res.locals;
+      const { title, content, privateOption } = await postInputPattern.validateAsync(req.body);
+      await this.postsService.createPost(title, content, privateOption, userId);
+      return res.status(201).json({ message: '게시글 작성 성공' });
+    } catch (err) {
+      return next(err);
+    }
+  };
+
+  getAllPosts = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const posts = await this.postsService.getPosts();
+      return res.status(200).json({ posts });
+    } catch (err) {
+      return next(err);
+    }
+  };
+
+  getDtailPost = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const postId = await postIdPattern.validateAsync(Number(req.params.postId));
+      const post = await this.postsService.getPost(postId);
+      return res.status(200).json({ post });
+    } catch (err) {
+      return next(err);
+    }
+  };
+
+  getWrittenPost = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const postId = await postIdPattern.validateAsync(Number(req.params.postId));
+      const { userId } = res.locals;
+      const post = await this.postsService.getWrittenPost(postId, userId);
+      return res.status(200).json({ post });
+    } catch (err) {
+      return next(err);
+    }
+  };
+
+  updatePost = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { userId } = res.locals;
+      const postId = await postIdPattern.validateAsync(Number(req.params.postId));
+      const { title, content, privateOption } = await postInputPattern.validateAsync(req.body);
+      await this.postsService.updatePost(title, content, privateOption, userId, postId);
+      return res.status(200).json({ message: '수정 완료' });
+    } catch (err) {
+      return next(err);
+    }
+  };
+
+  deletePost = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { userId } = res.locals;
+      const postId = await postIdPattern.validateAsync(Number(req.params.postId));
+      await this.postsService.deletePost(userId, postId);
+      return res.status(200).json({ message: '삭제 완료' });
+    } catch (err) {
+      return next(err);
+    }
+  };
 }
 
 export default PostsController;
--- src/services/posts.service.ts
+++ src/services/posts.service.ts
@@ -1,5 +1,6 @@
-import prisma from '../config/databases/prisma';
+import { badRequest, forbidden } from '@hapi/boom';
 import PostsRepository from '../repositories/posts.repository';
+import prisma from '../config/databases/prisma';
 
 class PostsService {
   postsRepository: PostsRepository;
@@ -7,6 +8,59 @@ class PostsService {
   constructor() {
     this.postsRepository = new PostsRepository(prisma);
   }
+
+  createPost = async (title: string, content: string, privateOption: number, userId: number) => {
+    await this.postsRepository.createPost(title, content, privateOption, userId);
+  };
+
+  getPosts = async () => {
+    const posts = await this.postsRepository.getPosts();
+    return posts;
+  };
+
+  getPost = async (postId: number) => {
+    const post = await this.postsRepository.getPost(postId);
+
+    if (!post) throw badRequest('존재하지 않는 게시글');
+
+    return post;
+  };
+
+  getWrittenPost = async (postId: number, userId: number) => {
+    const writtenPost = await this.postsRepository.getWrittenPost(postId);
+
+    if (!writtenPost) throw badRequest('존재하지 않는 게시글');
+
+    if (writtenPost.userId !== userId) throw forbidden('사용자 정보 불일치');
+
+    return writtenPost;
+  };
+
+  updatePost = async (
+    title: string,
+    content: string,
+    privateOption: number,
+    userId: number,
+    postId: number
+  ) => {
+    const existPost = await this.postsRepository.findOneByPostId(postId);
+
+    if (!existPost) throw badRequest('존재하지 않는 게시글');
+
+    if (existPost.userId !== userId) throw forbidden('사용자 정보 불일치');
+
+    await this.postsRepository.updatePost(postId, title, content, privateOption);
+  };
+
+  deletePost = async (userId: number, postId: number) => {
+    const existPost = await this.postsRepository.findOneByPostId(postId);
+
+    if (!existPost) throw badRequest('존재하지 않는 게시글');
+
+    if (existPost.userId !== userId) throw forbidden('사용자 정보 불일치');
+
+    await this.postsRepository.deletePost(postId);
+  };
 }
 
 export default PostsService;
